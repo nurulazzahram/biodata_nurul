fe8e23f228fc4685988e3c08cbd5cb2f07880f5b
nurulazzahram <nurulazahraaz@gmail.com>
2025-07-29 20:28:32 +0700

    Add files via upload

---
+++ biodata.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "Nurul Azzahra Maulida";
+    string ttl = "Cianjur, 07 Maret 2010";
+    string alamat = "Jl. Jatiwangi, Desa Karangharja, Kecamatan Pagelaran"; 
+    string asal_sekolah = "Al Musyarofah";
+    
+    
+    cout << "=======================================================" << endl;
+    cout << "                       BIODATA DIRI       " << endl;
+    cout << "=======================================================" << endl;
+    cout << "Nama         : " << nama << endl;
+    cout << "TTL          : " << ttl << endl;
+    cout << "Alamat       : " << alamat << endl;
+    cout << "Asal Sekolah : " << asal_sekolah << endl;
+    cout << "=======================================================" << endl;
+
+    return 0;
+}
